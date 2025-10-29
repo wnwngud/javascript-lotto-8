3066816275298f40fa41d89b002c5aacc39b60f9
Ju Hyeong Suh <blane7777@naver.com>
2025-10-29 18:45:29 +0900

    feat(App): add insert Coin feat with test and catching Exception

---
+++ __tests__/ValidCoinTest.js
@@ -0,0 +1,14 @@
+import { validCoin } from "../src/App.js"
+
+describe.each([
+    [1000, 0],
+    [' ', 1]
+    ['abc', 2],
+    ['100a', 2],
+    [1005, 3]
+])("validCoin(%s)", (coin, caseNumber) => {
+    if (caseNumber === 0)
+        it("금액 입력 양식 검사: 정상", () => { expect(validCoin(coin)).toBe(true); });
+    if ([1, 2, 3].includes(caseNumber))
+        it("금액 입력 양식 검사: 비정상 입력", () => {expect(()=> validCoin(coin)).toThrow("[ERROR] 1000원 단위의 숫자만 입력해 주세요.")})
+});
--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-class App {
-  async run() {}
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+async function insertCoin() {
+  let coin = await MissionUtils.Console.readLineAsync("구입 금액을 입력해 주세요.\n");
+
+  if (validCoin(coin)) return Number(coin);
 }
 
-export default App;
+function validCoin(coin) {
+  let num = Number(coin);
+
+  if (coin === '' || isNaN(num) || num % 1000 != 0) {
+    throw new Error("[ERROR] 1000원 단위의 숫자만 입력해 주세요.")
+  }
+
+  return true;
+}
+
+class App {
+  async run() {
+    const COIN = await insertCoin();
+    MissionUtils.Console.print(COIN);
+  }
+}
+
+export default { App, validCoin };
