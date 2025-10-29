884cf51e0e5ef3b1f148d847d77a63176cc99278
Ju Hyeong Suh <blane7777@naver.com>
2025-10-29 19:31:40 +0900

    feat(App): add printing the number of lotto

--- src/App.js
+++ src/App.js
@@ -10,17 +10,24 @@ function validCoin(coin) {
   let num = Number(coin);
 
   if (coin === '' || isNaN(num) || num % 1000 != 0) {
-    throw new Error("[ERROR] 1000원 단위의 숫자만 입력해 주세요.")
+    throw new Error("[ERROR] 1000원 단위의 숫자만 입력해 주세요.");
   }
 
   return true;
 }
 
+function printNumOfLotto(coin) {
+  let count = coin / 1000;
+
+  MissionUtils.Console.print(`\n${count}개를 구매했습니다.`)
+  return count;
+}
+
 class App {
   async run() {
     const COIN = await insertCoin();
-    MissionUtils.Console.print(COIN);
+    const COUNT = printNumOfLotto(COIN);
   }
 }
 
-export default { App, validCoin };
+export { App, validCoin };
