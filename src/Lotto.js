dd8d21bac116b3ffcad64439805769201e8c2fc7
wnwngud <blane777@naver.com>
2025-10-30 15:53:12 +0900

    feat(Lotto): add get how many match feature with test

--- src/Lotto.js
+++ src/Lotto.js
@@ -13,6 +13,17 @@ class Lotto {
   }
 
   // TODO: 추가 기능 구현
+  getHowManyMatch(lotto, matchCount, bonusNumber) {
+    let matchNums = lotto.filter(it => this.#numbers.includes(it));
+    let matchLength = matchNums.length;
+    let haveBonusNumber = lotto.includes(bonusNumber);
+
+    if (matchLength === 6) matchCount[4] += 1;
+    else if (matchLength === 5 && haveBonusNumber) matchCount[3] += 1;
+    else if (matchLength === 5) matchCount[2] += 1;
+    else if (matchLength === 4) matchCount[1] += 1;
+    else if (matchLength === 3) matchCount[0] += 1;
+  }
 }
 
 export default Lotto;
